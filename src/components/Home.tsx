730a1d37e43658b6761a95ef4ecb397f3b3e7666
koushik-10 <koushik@mudrantar.com>
2023-10-11 00:07:11 +0530

    added logo to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Searchbar from "./Searchbar";
 import bgImg from "../assets/images/homebg.jpg";
+import logoImg from "../assets/images/zevilogo.png";
 
 const Home = () => {
   return (
     <div className="relative h-screen">
+      <img src={logoImg} alt="Logo" className="absolute top-11 right-11 w-20" />
       <div
         className="bg-cover bg-no-repeat h-full flex justify-center items-center object-contain"
         style={{
